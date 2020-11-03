339c4923ebab8efc79aae67abce46b3810072900
Federico Rubino <federico.rubino8@gmail.com>
2020-11-03 09:02:42 -0800

    initial set up

---
+++ app/src/main/java/com/example/cst438_meditationapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cst438_meditationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
