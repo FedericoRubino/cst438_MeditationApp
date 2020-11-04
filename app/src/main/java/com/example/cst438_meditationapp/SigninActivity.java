d7eb8a2311a54d83edfb1206adca03a326d3f185
annastublerdev <annastubler@gmail.com>
2020-11-04 13:24:38 -0800

    Some activities

--- app/src/main/java/com/example/cst438_meditationapp/SigninActivity.java
+++ app/src/main/java/com/example/cst438_meditationapp/SigninActivity.java
@@ -3,15 +3,12 @@ package com.example.cst438_meditationapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
-
-    Button loginBtn;
+public class SigninActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signin);
     }
 }
