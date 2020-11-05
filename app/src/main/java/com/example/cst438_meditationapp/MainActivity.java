2f35ae835d61a58763ff8a6504afd8b2bb68a8a3
redn6gx <bodavis@csumb.edu>
2020-11-05 15:59:59 -0800

    set up an example recycler view

--- app/src/main/java/com/example/cst438_meditationapp/MainActivity.java
+++ app/src/main/java/com/example/cst438_meditationapp/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.cst438_meditationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button testButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        //*************TEST RECYCLER VIEW*************
+        testButton = findViewById(R.id.Test_Button);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RecyclerViewActivity.class);
+                startActivity(intent);
+            }
+        });
+        //*************TEST RECYCLER VIEW*************
+
+
+
     }
 }
