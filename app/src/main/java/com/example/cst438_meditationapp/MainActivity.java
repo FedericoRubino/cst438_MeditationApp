2a116c30c9aa5fe1962e28a871b01bac78e6bf22
Federico Rubino <federico.rubino8@gmail.com>
2020-11-06 21:44:10 -0800

    Set up the database, signup page

--- app/src/main/java/com/example/cst438_meditationapp/MainActivity.java
+++ app/src/main/java/com/example/cst438_meditationapp/MainActivity.java
@@ -1,21 +1,39 @@
 package com.example.cst438_meditationapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+
+import static android.content.ContentValues.TAG;
+
 public class MainActivity extends AppCompatActivity {
 
     Button testButton;
+    // Access a Cloud Firestore instance from your Activity
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
 
 
         //*************TEST RECYCLER VIEW*************
@@ -29,7 +47,57 @@ public class MainActivity extends AppCompatActivity {
         });
         //*************TEST RECYCLER VIEW*************
 
+        readFromUserDB();
+    }
+
+    public void startSignInActivity(View view){
+        Intent intent = SigninActivity.getIntent(this.getApplicationContext(),"");
+        startActivity(intent);
+    }
 
 
+
+
+    public void readFromUserDB(){
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
+    }
+
+    public void addUser(){
+
+        // Create a new user with a first, middle, and last name
+        HashMap<String, Object> user2 = new HashMap<>();
+        user2.put("first", "Alan");
+        user2.put("middle", "Mathison");
+        user2.put("last", "Turing");
+        user2.put("born", 1912);
+
+        // Add a new document with a generated ID
+        db.collection("users")
+                .add(user2)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Error adding document", e);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cst438_meditationapp/SigninActivity.java
+++ app/src/main/java/com/example/cst438_meditationapp/SigninActivity.java
@@ -2,6 +2,7 @@ package com.example.cst438_meditationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,9 +12,19 @@ import android.widget.Toast;
 
 public class SigninActivity extends AppCompatActivity {
 
+    public static final String EXTRA = "SIGNIN EXTRA";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
     }
+
+
+    public static Intent getIntent(Context context, String val){
+        Intent intent = new Intent(context, SigninActivity.class);
+        intent.putExtra(EXTRA, val);
+        return intent;
+    }
+
 }
