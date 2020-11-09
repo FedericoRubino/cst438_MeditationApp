757bf9e1496e5291dc439ed0946e88bb6849c845
Federico Rubino <federico.rubino8@gmail.com>
2020-11-08 21:58:54 -0800

    connected the feed page with the database

--- app/src/main/java/com/example/cst438_meditationapp/FeedActivity.java
+++ app/src/main/java/com/example/cst438_meditationapp/FeedActivity.java
@@ -1,5 +1,6 @@
 package com.example.cst438_meditationapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,17 +9,32 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
 
 public class FeedActivity extends AppCompatActivity {
-    List<String> objectArray = Arrays.asList("OBJ-1", "OBJ-2", "OBJ-3", "OBJ-4", "OBJ-5");
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    List<Map<String, Object>> objectArray;
+
+
     String selectedObject;
     FeedActivity.Adapter adapter;
 
@@ -30,13 +46,11 @@ public class FeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        objectArray = new ArrayList();
+        getDBInfo();
 
-        //holds objects
-        RecyclerView rvAssignment = findViewById(R.id.feed_recycler_view);
-        rvAssignment.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new FeedActivity.Adapter();
-        rvAssignment.setAdapter(adapter);
     }
+
 
     private class Adapter  extends RecyclerView.Adapter<FeedActivity.ItemHolder> {
 
@@ -61,15 +75,17 @@ public class FeedActivity extends AppCompatActivity {
             super(inflater.inflate(R.layout.feed_item, parent, false));
         }
 
-        public void bind(String obj) {
+        public void bind(Map<String, Object> obj) {
             final TextView item = itemView.findViewById(R.id.postTv);
-            item.setText(obj);
+            item.setText(obj.get("description").toString());
+            final TextView itemTitle = itemView.findViewById(R.id.titleTv);
+            itemTitle.setText(obj.get("name").toString());
             //make item clickable
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     //save selected object
-                    selectedObject = objectArray.get(getAdapterPosition());
+                    selectedObject = objectArray.get(getAdapterPosition()).get("username").toString();
                     Toast.makeText(FeedActivity.this, selectedObject, Toast.LENGTH_SHORT).show();
 
                     //change text color of selected item
@@ -87,6 +103,41 @@ public class FeedActivity extends AppCompatActivity {
         }
     }
 
+    // This gets the information from the database and then calls the
+    // recyclerview set up
+    public void getDBInfo(){
+        db.collection("posts")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Map<String, Object> object = document.getData();
+                                objectArray.add(object);
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+//                                Log.d(TAG, document.getId() + " => " + objectArray);
+                            }
+                            setUpRecyclerView();
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+    }
+
+    // Sets up the recyclerview!
+    public void setUpRecyclerView(){
+        if(objectArray.size() > 0 ) {
+            //holds objects
+            RecyclerView rvAssignment = findViewById(R.id.feed_recycler_view);
+            rvAssignment.setLayoutManager(new LinearLayoutManager(this));
+            adapter = new Adapter();
+            rvAssignment.setAdapter(adapter);
+        } else {
+            Toast.makeText(this, "Probelms!!!", Toast.LENGTH_LONG).show();
+        }
+    }
 
 
     // Intent factory
