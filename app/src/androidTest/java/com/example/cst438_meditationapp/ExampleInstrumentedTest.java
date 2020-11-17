9e37791da37a1042dbd9b31d7fa13e2bb8ca28a7
annastublerdev <annastubler@gmail.com>
2020-11-17 13:34:24 -0800

    Testing

--- app/src/androidTest/java/com/example/cst438_meditationapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/cst438_meditationapp/ExampleInstrumentedTest.java
@@ -1,16 +1,8 @@
 package com.example.cst438_meditationapp;
 
-import android.content.Context;
-
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
 
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.HashMap;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,23 +11,25 @@ import java.util.HashMap;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    private FirebaseFirestore db;
-
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        db = FirebaseFirestore.getInstance();
-    }
-
-    @Test
-    public void testUsername() {
-        HashMap<String, Object> newUser = new HashMap<>();
-        newUser.put("username", "cst");
-        newUser.put("password", "cst123");
-        Util.addUserToDB(db, newUser);
-//        User retrieveUser = getUserFromDB(FirebaseFirestore db, User newUser);
-//        assertEquals(newUser.getUsername(), retrieveUser.getUsername());
-//        assertNotEquals("abc", newUser.getUsername());
-    }
+//    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+//    private HashMap<String, Object> newUser;
+//    @Before
+//    public void setup(){
+//        newUser = new HashMap<>();
+//        newUser.put("username", "cst");
+//        newUser.put("password", "cst123");
+//        Util.addUserToDB(db, newUser);
+//    }
+//
+//    @Test
+//    public void useAppContext() {
+//        // Context of the app under test.
+//        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+//    }
+//
+//    @Test
+//    public void testUsername() {
+//        assertTrue(Util.checkForUserInDB(db, newUser));
+//        assertNotEquals("abc", newUser.get("username"));
+//    }
 }
