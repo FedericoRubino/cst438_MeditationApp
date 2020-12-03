5458412b772f1c188bfcc97fbedbe555bbc3bb22
annastublerdev <annastubler@gmail.com>
2020-12-03 14:55:05 -0800

    Menu

--- app/src/main/java/com/example/cst438_meditationapp/Home.java
+++ app/src/main/java/com/example/cst438_meditationapp/Home.java
@@ -3,11 +3,13 @@ package com.example.cst438_meditationapp;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class Home extends AppCompatActivity {
 
@@ -17,8 +19,15 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        TextView username = findViewById(R.id.currentUser);;
-        username.setText(LoginActivity.loggedUser);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_nav, menu);
+        return true;
     }
 
     public void startMeditation2(View view){
